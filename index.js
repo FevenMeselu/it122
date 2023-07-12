e96ffc636fd02c447b7de9617589e29137a20e70
Replit user <>
2023-07-12 02:28:31 +0000

    Homework 1 complete

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const http = require("http")
 http.createServer((req, res) => {
   var path = req.url.toLowerCase();
+  console.log(path)
   switch (path) {
     case '/':
       res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end("This is my home page")
       break;
     case '/about':
       res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Not found');
+      res.end('I am a studet at SCC');
       break;
-  }).listen(process.env.PORT || 3000);
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not foud');
+      break;
+  }
+ }).listen(process.env.PORT || 3000);
